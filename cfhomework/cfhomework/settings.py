76dac4ce5e038d36bdc634d4ae7e32b689ada2f1
Vikas Thakur <vthakur@vthakur-MBA13.local>
2014-07-14 21:52:33 -0700

    minor compilation fix for heroku

--- cfhomework/cfhomework/settings.py
+++ cfhomework/cfhomework/settings.py
@@ -80,3 +80,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_PATH,'static')
